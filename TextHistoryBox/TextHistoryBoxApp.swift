5fe3567d66946cc90096b24a25f1a1ed9203bcf2
devsungmin <kimsungmin.dev@gmail.com>
2026-02-19 14:35:24 +0900

    Initial Commit

---
+++ TextHistoryBox/TextHistoryBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextHistoryBoxApp.swift
+//  TextHistoryBox
+//
+//  Created by 김성민 on 2/19/26.
+//
+
+import SwiftUI
+
+@main
+struct TextHistoryBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
